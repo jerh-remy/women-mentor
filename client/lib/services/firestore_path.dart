b40d359632709b4be1af319b2510b73e696d9236
Samantha Tung <samantha.tungling@gmail.com>
2021-03-27 17:04:21 +0000

    Add bookings and users collectionStream

--- client/lib/services/firestore_path.dart
+++ client/lib/services/firestore_path.dart
@@ -1,8 +1,9 @@
 class FirestorePath {
+  static String users() => 'users';
   static String user(String uid) => 'users/$uid';
   static String booking(String uid, String bookingId) =>
       'users/$uid/jobs/$bookingId';
-  static String bookings(String uid) => 'users/$uid/bookings';
+  static String bookings() => 'bookings';
   static String posts(String postId) => 'posts/$postId';
   static String commentsOnPosts(String postId, String commentId) =>
       'posts/$postId/comments/$commentId';
